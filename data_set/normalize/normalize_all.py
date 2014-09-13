7174a5b8a4de9177a674fec6f51a3f7af921cd91
Adam Polyak <adampolyak@gmail.com>
2014-09-14 00:10:28 +0300

    restored commented out code

--- data_set/normalize/normalize_all.py
+++ data_set/normalize/normalize_all.py
@@ -12,10 +12,10 @@ def main(data_dir, dst_dir):
         os.mkdir(dst_dir)
     data_dir = os.path.abspath(data_dir)
 
-    #lfw.main(os.path.join(data_dir, 'lfw'), dst_dir)  # <name>_<id>.jpg
-    #pubfig83.main(os.path.join(data_dir, 'pubfig83'), dst_dir)  # <image id>.jpg
-    #pubfig.main(os.path.join(data_dir, 'pubfig'), dst_dir)  # <md5>.jpg
-    #SUFR.main(os.path.join(data_dir, 'SUFRData'), dst_dir)  # <image id>.jpg
+    lfw.main(os.path.join(data_dir, 'lfw'), dst_dir)  # <name>_<id>.jpg
+    pubfig83.main(os.path.join(data_dir, 'pubfig83'), dst_dir)  # <image id>.jpg
+    pubfig.main(os.path.join(data_dir, 'pubfig'), dst_dir)  # <md5>.jpg
+    SUFR.main(os.path.join(data_dir, 'SUFRData'), dst_dir)  # <image id>.jpg
     morph.main(os.path.join(data_dir, 'morph'), dst_dir)  # morph_<id>_<image_no>.jpg
 
 if __name__ == "__main__":
