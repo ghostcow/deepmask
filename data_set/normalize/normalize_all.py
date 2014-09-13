062db78502ec494b8a1451f9a36325c9f08057e4
Adam Polyak <adampolyak@gmail.com>
2014-09-13 23:41:34 +0300

    pubfig83 uses same directory naming format as lfw

--- data_set/normalize/normalize_all.py
+++ data_set/normalize/normalize_all.py
@@ -11,10 +11,10 @@ def main(data_dir, dst_dir):
         os.mkdir(dst_dir)
     data_dir = os.path.abspath(data_dir)
 
-    lfw.main(os.path.join(data_dir, 'lfw'), dst_dir)
-    pubfig83.main(os.path.join(data_dir, 'pubfig83'), dst_dir)
-    pubfig.main(os.path.join(data_dir, 'pubfig'), dst_dir)
-    SUFR.main(os.path.join(data_dir, 'SUFRData'), dst_dir)
+    lfw.main(os.path.join(data_dir, 'lfw'), dst_dir)  # <name_id>.jpg
+    pubfig83.main(os.path.join(data_dir, 'pubfig83'), dst_dir)  # <id>.jpg
+    pubfig.main(os.path.join(data_dir, 'pubfig'), dst_dir)  # <md5>.jpg
+    SUFR.main(os.path.join(data_dir, 'SUFRData'), dst_dir)  # <id>.jpg
 
 if __name__ == "__main__":
     main('/Users/adamp/Research/Data', '/tmp/normalized_data')
--- data_set/normalize/pubfig83.py
+++ data_set/normalize/pubfig83.py
@@ -1,8 +1,13 @@
 import distutils.core
+import os
 
 
 def main(src_dir, dst_dir):
-    distutils.dir_util.copy_tree(src_dir, dst_dir)
+    images_dirs_path = os.listdir(src_dir)
+    for image_dir_path in images_dirs_path:
+        if not "." in image_dir_path:
+            distutils.dir_util.copy_tree(os.path.join(src_dir, image_dir_path),
+                                         os.path.join(dst_dir, image_dir_path.replace(' ', '_')))
 
 if __name__ == '__main__':
     main()
