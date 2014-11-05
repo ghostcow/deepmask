ca17b3a4a37cb28edab8070d82d151ed28aa1b57
Adam Polyak <adampolyak@gmail.com>
2014-11-06 00:04:30 +0200

    added code for using mexopencv. note that mexopencv needs to be in path: addpath('~/path/to/mexopencv') Still need to verify that detector outputs the same results as OpenCV_ViolaJones detector (might be off by 1)

--- img_preproc/facedet/runfacedet.m
+++ img_preproc/facedet/runfacedet.m
@@ -1,23 +1,28 @@
 function DETS = runfacedet(I,imgpath)
-
-tmppath=tempname;
-pgmpath=[tmppath '.pgm'];
-if nargin<2
-    detpath=[tmppath '.vj'];
-else
-    detpath=[imgpath '.vj'];
-end
-
-if ~exist(detpath, 'file')
-    % run this only if the detections file doesn;t exist from previous run
-    imwrite(I,pgmpath);
-
     root=fullfile(fileparts(which(mfilename)), 'OpenCV_ViolaJones');
-    system(sprintf('%s/Release/OpenCV_ViolaJones %s/haarcascade_frontalface_alt.xml "%s" "%s"',root,root,pgmpath,detpath));
-    delete(pgmpath);
-end
+    detectorConf=fullfile(root,'haarcascade_frontalface_alt.xml');
+    
+    detector = cv.CascadeClassifier(detectorConf);
+    boxes = detector.detect(I, ...
+                            'ScaleFactor',  1.1, ...
+                            'MinNeighbors', 2, ...
+                            'MinSize', [40, 40]);
 
-DETS=readfacedets(detpath);
-if nargin<2
-    delete(detpath);
-end
+    
+    if size(boxes,2) > 0
+        T=zeros(size(boxes,2)+1,4);
+        T(1,1)=size(boxes,2);
+        T(2:end,:)=reshape(cell2mat(boxes),[4,size(boxes,2)])';
+        
+        widthVec=T(2:end,3);
+        heightVec=T(2:end,4);
+        T(2:end,3)=T(2:end,2);
+        T(2:end,4)=heightVec+T(2:end,3);
+        T(2:end,2)=widthVec+T(2:end,1);
+        
+        BB=T(2:end,:)';
+        DETS=[(BB(1,:)+BB(2,:))/2 ; (BB(3,:)+BB(4,:))/2 ; (BB(2,:)-BB(1,:))/2];
+        DETS(4,:)=1;
+    else
+        DETS=[];
+    end
