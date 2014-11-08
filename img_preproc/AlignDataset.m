efc30848ffd7455b7068496fe205263b0ff3aee4
Adam Polyak <adampolyak@gmail.com>
2014-11-08 23:59:39 +0200

    Revert "committing code used for debugging - will be reverted in next commit"
    This reverts commit c077ab2ebdf559aad9a1f8d3d7249baa2cc8f331.

--- img_preproc/AlignDataset.m
+++ img_preproc/AlignDataset.m
@@ -5,10 +5,10 @@ currDir = fileparts(mfilename('fullpath'));
 run(fullfile(currDir, 'init.m'));
 
 %% Define all paths here
-mainDir = '/tmp/pubfig/images';
-allImagesDir = mainDir;
-alignedImagesDir = fullfile(mainDir, '../aligned_mexopencv');
-detectionsFileName = '/tmp/pubfig/detections_pubfig_mexopencv.txt';
+mainDir = '/media/data/datasets/pubfig';
+allImagesDir = fullfile(mainDir, 'images');
+alignedImagesDir = fullfile(mainDir, 'aligned');
+detectionsFileName = 'detections_pubfig.txt';
 
 % whether to take only the central face or all faces
 useCenterFace = false;
@@ -16,8 +16,7 @@ isOverWrite = true;
 
 %%
 figDirs = dir(allImagesDir);
-figDirs = figDirs(4:end);
-%nPersons = 3;
+figDirs = figDirs(3:end);
 nPersons = length(figDirs);
 noFacesCounter = 0;
 
@@ -43,9 +42,9 @@ for iFigure = 1:nPersons
             continue;
         end
         try
-            [detection, landmarks, aligned_imgs, det2] = align_face(opts, imPath);
+            [detection, landmarks, aligned_imgs] = align_face(opts, imPath);
         catch me
-            %fprintf('%s - Error - %s\n', imPath, me.message);
+            fprintf('%s - Error - %s\n', imPath, me.message);
         end
 
         nFaces = length(aligned_imgs);
@@ -56,13 +55,9 @@ for iFigure = 1:nPersons
         
         if (nFaces == 1)
             imwrite(im2double(aligned_imgs{1}), alignedImagePath);   
-            fprintf(fid, '%s,%s\n', imPath, alignedImagePath);
-            fprintf(fid, '%d %d %d %d\n', ...
+            fprintf(fid, '%s,%s,%d %d %d %d\n', imPath, alignedImagePath, ...
                     detection(1, 1), detection(2, 1), ...
                     detection(3, 1), detection(4, 1));  
-            fprintf(fid, '%d %d %d %d\n', ...
-                    det2(1, 1), det2(2, 1), ...
-                    det2(3, 1), det2(4, 1));  
         else
             if useCenterFace
                 % look for the central face
@@ -82,13 +77,9 @@ for iFigure = 1:nPersons
                     alignedImagePath = fullfile(outputDir, ...
                         [images(iImage).name(1:end-4) '.' num2str(iFace) '.jpg']);
                     imwrite(im2double(aligned_imgs{iFace}), alignedImagePath);
-                    fprintf(fid, '%s,%s\n', imPath, alignedImagePath);
-                    fprintf(fid, '%d %d %d %d\n', ...
-                            detection(1, iFace), detection(2, iFace), ...
-                            detection(3, iFace), detection(4, iFace));  
-                    fprintf(fid, '%d %d %d %d\n', ...
-                            det2(1, iFace), det2(2, iFace), ...
-                            det2(3, iFace), det2(4, iFace));                   
+                    fprintf(fid, '%s,%s,%d %d %d %d\n', imPath, alignedImagePath, ...
+                                 detection(1, iFace), detection(2, iFace), ...
+                                 detection(3, iFace), detection(4, iFace));                   
                 end
             end
         end
--- img_preproc/align_face.m
+++ img_preproc/align_face.m
@@ -1,4 +1,4 @@
-function [detections, landmarks, aligned_imgs, detections2] = align_face(opts, imgPath)
+function [detections, landmarks, aligned_imgs] = align_face(opts, imgPath)
 %ALIGN_FACE Summary of this function goes here
 %   Detailed explanation goes here
     [I, map] = imread(imgPath);
@@ -12,32 +12,6 @@ function [detections, landmarks, aligned_imgs, detections2] = align_face(opts, i
     end
     
     detections = runfacedet(I, imgPath);
-    detections2 = runfacedet2(I, imgPath);
-    
-    
-    if size(detections, 2) ~= size(detections2, 2) && ...
-       size(detections2, 2)~=0   
-        imshow(I);
-        for i=1:size(detections, 2)
-            rectangle('Position', det_to_rec(detections(:,i)), 'EdgeColor', 'g');
-        end
-        
-        for i=1:size(detections2, 2)
-            rectangle('Position', det_to_rec(detections2(:,i)), 'EdgeColor', 'r');
-        end
-        waitforbuttonpress
-        
-        fprintf('found different number of faces in images: %s\n', imgPath);
-    end
-    
-    for i=1:size(detections, 2)
-        for j=1:1:size(detections, 2)
-            if detections2(j,i) - detections(j,i) > 2
-               fprintf('detection is different by more than 1 pixel for: %s\n', imgPath); 
-            end
-        end
-    end
-    
     nFaces = size(detections, 2);
     
     % assume there is a single face in the image
