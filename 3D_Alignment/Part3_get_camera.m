9b71037c80641216d1ab3b5c0d10502214dbf76d
Adam Polyak <adampolyak@gmail.com>
2014-12-01 22:08:08 +0200

    now x2d size isn't constant

--- 3D_Alignment/Part3_get_camera.m
+++ 3D_Alignment/Part3_get_camera.m
@@ -1,7 +1,8 @@
 function P = Part3_get_camera(x2d, landmarks3D, cov_mat)    
     % construct X3d
-    X3d=zeros(68*2, 8);
-    for i=1:68
+    no_samples=size(x2d,1)/2;
+    X3d=zeros(no_samples*2, 8);
+    for i=1:no_samples
         curr_ind = 2*(i-1)+1;
         X3d(curr_ind:curr_ind+1,:)=[landmarks3D(i,:) 1 zeros(1,4); ...
                                     zeros(1,4) landmarks3D(i,:) 1];
