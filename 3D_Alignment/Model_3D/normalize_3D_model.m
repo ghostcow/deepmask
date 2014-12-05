4fb9095eb9c0a1ac673a6c390ba4457ca01ecefc
Adam Polyak <adampolyak@gmail.com>
2014-12-05 19:25:02 +0200

    normalise now receives range for x and y

--- 3D_Alignment/Model_3D/normalize_3D_model.m
+++ 3D_Alignment/Model_3D/normalize_3D_model.m
@@ -1,4 +1,4 @@
-function xy_3d = normalize_3D_model(xyz_3d, scale)
+function xy_3d = normalize_3D_model(xyz_3d, xlimits, ylimits)
     % normalize x,y coordinates from the 3D model
     xy_3d = xyz_3d([1 3],:);
     xy_3d_min = min(xy_3d, [], 2);
@@ -7,9 +7,12 @@ function xy_3d = normalize_3D_model(xyz_3d, scale)
     xy_3d = bsxfun(@minus, xy_3d, xy_3d_min);
     xy_3d = bsxfun(@times, xy_3d, 1 ./ (xy_3d_max - xy_3d_min));
 
-    xy_3d = bsxfun(@times, xy_3d, scale);
-    % vertical flip of  x,y coordinates
-    xy_3d(1,:) = scale(1) - xy_3d(1,:); 
-    xy_3d(2,:) = scale(2) - xy_3d(2,:);  
+     % vertical flip of  x,y coordinates
+    xy_3d = 1 - xy_3d; 
+    
+    % scale to new limits
+    xy_3d = bsxfun(@times, xy_3d, [xlimits(2) - xlimits(1) + 1; ...
+                                   ylimits(2) - ylimits(1) + 1]);
+    xy_3d = bsxfun(@plus, xy_3d, [xlimits(1) ; ylimits(1)]);
 end
 
