12dddb5fde3b0617490647ad8eaa13b6d9c086c1
Adam Polyak <adampolyak@gmail.com>
2015-09-10 16:10:22 +0300

    Added BN models, improved init code

--- nn/model.lua
+++ nn/model.lua
@@ -29,17 +29,18 @@ else
 end
 
 -- 2. Initilize model according to MSR
-print('=> Initializing weights according to PReLU')
-for i=1,#model.modules do
-    local layer = model:get(i)
-    if layer.weight ~= nil and layer.kW ~= nil then
-        local stdv = math.sqrt(2/(1.25*layer.kW*layer.kH*layer.nInputPlane))
-        layer.weight:normal(0, stdv)
+print('=> Initializing weights according to MSR')
+local function MSRinit(net)
+    local function init(name)
+        for _,v in pairs(net:findModules(name)) do
+            local n = v.kW*v.kH*v.nOutputPlane
+            v.weight:normal(0,math.sqrt(2/(1.25*n))) -- 1.25 is the initial value
+            v.bias:zero()
+        end
     end
-    if layer.bias ~= nil then
-        layer.bias:fill(0)
-    end
+    init'nn.SpatialConvolution'
 end
+MSRinit(model)
 
 -- 3. Create Criterion
 criterion = nn.ClassNLLCriterion()
