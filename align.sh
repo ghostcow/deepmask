feca7ed81179cf08b2f8a7e18ab9525a88544ac9
adampolyak <Adampolyak@gmail.com>
2015-09-10 13:54:30 +0300

    new dlib alignment

--- align.sh
+++ align.sh
@@ -4,15 +4,12 @@ cd 2D_Alignment
 MATLAB_PATH="/usr/local/MATLAB/R2013a/bin/matlab"
 
 # alignment script parameters
-MAIN_DIR="/media/data/datasets/FaCeleb"
-IMAGES_DIR="/media/data/datasets/FaCeleb/images"
+MAIN_DIR="/home/adampolyak/datasets/CASIA"
+IMAGES_DIR="${MAIN_DIR}/images"
 DETECT_FILE="detections.txt"
 # scratch generation script parameters
 DEEPID_DIR="$MAIN_DIR/aligned_deepid"
-SCRATCH_DIR="$MAIN_DIR/aligned_scratch"
 
 ALIGN_CMD="addpath('/media/data/projects/mexopencv/');mainDir='$MAIN_DIR';allImagesDir='$IMAGES_DIR';detectionsFileName='$DETECT_FILE';AlignDataset;exit"
-GENERATE_SCRATCH_CMD="mainDir='$MAIN_DIR';srcDir='$DEEPID_DIR';dstDir='$SCRATCH_DIR';FLIP=false;GenerateScratchFormat;exit"
 
 $MATLAB_PATH -nodesktop -nojvm -nosplash -r $ALIGN_CMD
-$MATLAB_PATH -nodesktop -nojvm -nosplash -r $GENERATE_SCRATCH_CMD
