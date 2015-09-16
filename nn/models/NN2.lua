30bcd728803dd40a61407eeaa60f7790fa8d1acf
adampolyak <Adampolyak@gmail.com>
2015-09-16 11:57:05 +0300

    using simple ReLU

--- nn/models/NN2.lua
+++ nn/models/NN2.lua
@@ -10,39 +10,39 @@ local model = nn.Sequential()
 
 -- Conv11 & Conv12 & Pool1
 model:add(cudnn.SpatialConvolution(3, 64, 3, 3, 1, 1, 1, 1)) --1
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(64, 128, 3, 3, 1, 1, 1, 1)) --3
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
 
 -- Conv21 & Conv22 & Pool2
 model:add(cudnn.SpatialConvolution(128, 64, 3, 3, 1, 1, 1, 1)) -- 6
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(64, 128, 3, 3, 1, 1, 1, 1)) -- 8
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
 
 -- Conv31 & Conv32 & Pool 4
 model:add(cudnn.SpatialConvolution(128, 64, 3, 3, 1, 1, 1, 1)) -- 11
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(64, 128, 3, 3, 1, 1, 1, 1)) -- 13
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
 
 -- Conv41 & Conv42 & Conv43 & Pool 4
 model:add(cudnn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1, 1)) -- 16
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(128, 256, 3, 3, 1, 1, 1, 1)) -- 18
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(256, 256, 3, 3, 1, 1, 1, 1)) -- 20
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialMaxPooling(2, 2, 2, 2))
 
 -- Conv51 & Conv52 & Pool5
 model:add(cudnn.SpatialConvolution(256, 128, 3, 3, 1, 1, 1, 1)) -- 23
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(128, 256, 3, 3, 1, 1, 1, 1)) -- 25
-model:add(nn.PReLU())
+model:add(cudnn.ReLU(true))
 model:add(cudnn.SpatialConvolution(256, 256, 3, 3, 1, 1, 1, 1)) -- 27
 model:add(cudnn.SpatialAveragePooling(2, 2, 2, 2))
 
