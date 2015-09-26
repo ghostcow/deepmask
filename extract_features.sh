eea60e0597817c1a9aa6d764d03e5c66d7cdd91a
Adam Polyak <adampolyak@gmail.com>
2015-09-26 11:33:58 +0300

    using best model...

--- extract_features.sh
+++ extract_features.sh
@@ -4,8 +4,8 @@ RESULT_DIR=~/results/fitnet_1/KD/results_4
 DATASET=~/results/teacher_new_init/results/dataset.t7
 
 cd recognition
-th extract_features.lua --modelPath $RESULT_DIR/nn/model.net \
+th extract_features.lua --modelPath $RESULT_DIR/nn/model_best.net \
 			            --outputPath $RESULT_DIR/features \
 			            --dataPath $DATASET
-th extract_features_lfw.lua  --modelPath $RESULT_DIR/nn/model.net \
+th extract_features_lfw.lua  --modelPath $RESULT_DIR/nn/model_best.net \
 			                 --outputPath $RESULT_DIR/features
